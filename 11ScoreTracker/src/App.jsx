c8a428df006e22f364ce47162533493e51816ab6
Shivam Kumar <heyshiivam556@gmail.com>
2026-03-27 16:51:43 +0530

    feat: initialize 11ScoreTracker project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build
    - Created App.css for styling with Tailwind CSS
    - Implemented main App component with a search bar and score display
    - Added index.css for global styles and theming
    - Set up main entry point (main.jsx) for rendering the App component
    - Configured Vite with React and Tailwind CSS plugins

---
+++ 11ScoreTracker/src/App.jsx
@@ -0,0 +1,53 @@
+import { Search } from "lucide-react";
+import "./App.css";
+
+function App() {
+  
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-center">Score Tracker</h1>
+        <div className="searchbar flex flex-row mt-4 mx-auto">
+          <input
+            type="text"
+            className="w-64 h-10 p-2 bg-amber-50 text-black m-2 rounded "
+          />
+          <button>
+            <Search size={20} />
+          </button>
+        </div>
+        <div className=" flex justify-center mt-10">
+          <div className="match w-full text-center">
+            <div className=" bg-amber-900 ">
+              <span>Atlas</span> v/s <span>Nombre</span>
+            </div>
+            <ul className="scores flex justify-evenly">
+              <li className="absolute left-2">27-03</li>
+              <li>4</li>
+              <li>3</li>
+            </ul>
+            <ul className="scores flex justify-evenly">
+              <li className="absolute left-2">28-03</li>
+              <li>5</li>
+              <li>2</li>
+            </ul>
+            <div>
+              <div className="total mt-2">
+                <ul className=" flex justify-evenly">
+                  <li className="absolute left-2 ">Total</li>
+                  <li>3</li>
+                  <li>2</li>
+                  <li className="absolute right-4">3</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
