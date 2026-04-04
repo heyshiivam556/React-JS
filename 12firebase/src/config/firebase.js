45587d474cba74d2a11b33b157d2a332eac3253e
Shivam Kumar <heyshiivam556@gmail.com>
2026-04-04 14:28:35 +0530

    Update firebase.js

--- 12firebase/src/config/firebase.js
+++ 12firebase/src/config/firebase.js
@@ -6,13 +6,7 @@ import {getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0bO3KCKh0jRgQg1CzVA7QrtnRERTStl0",
-  authDomain: "fir-beginner-24192.firebaseapp.com",
-  projectId: "fir-beginner-24192",
-  storageBucket: "fir-beginner-24192.firebasestorage.app",
-  messagingSenderId: "113565801576",
-  appId: "1:113565801576:web:dc0d38aecb29e7a45fafdd",
-  measurementId: "G-8NGPMMRNVR"
+  apiKey: 
 };
 
 // Initialize Firebase
